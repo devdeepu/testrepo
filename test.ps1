48e99da51fe186907b3c49c0447dbb7e7b842d5a
devdeepu <76449558+devdeepu@users.noreply.github.com>
2020-12-24 15:54:16 +0530

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,3 +1,4 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Install-Module -Name Az -Force
+Install-Module -Name Az.Migrate -Force
